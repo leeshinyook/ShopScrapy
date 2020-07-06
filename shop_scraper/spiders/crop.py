857fe3bb32e803ced9d70b4102b703b0250cbb00
shinyooklee <tlsdbr97@gmail.com>
2020-07-06 18:04:44 +0900

    Add: Crop (lx, ly, rx, ry) images (Fix #2)

--- shop_scraper/spiders/crop.py
+++ shop_scraper/spiders/crop.py
@@ -1,30 +1,65 @@
-# from PIL import Image
-#
-# im = Image.open('')  # INPUT Image
-# pix = im.load()
-# width, height = im.size[0], im.size[1]
-# stand = pix[1, 1]
-#
-#
-# def find_border_top(start_x):
-#     for h in range(start_x):
-#         if pix[width / 2, h] != stand:
-#             return h
-#
-#
-# def find_border_bottom(start_x, start_y):
-#     cnt = 0
-#     for h in range(start_x, height):
-#         for w in range(start_y):
-#             if pix[w, h] == stand:
-#                 cnt += 1
-#         if cnt == width - 1:
-#             return h
-#         cnt = 0
-#
-#
-# crop_h1 = find_border_top(height)
-# crop_h2 = find_border_bottom(crop_x1 + 30, width - 1)
-# area = (1, crop_h1, width, crop_h2)
-# crop_img = im.crop(area)
-# crop_img.show()
+from urllib.request import urlopen
+from PIL import Image
+
+
+def find_images_location_from_url(url):
+    img = Image.open(urlopen(url))
+    width, height = img.size  # x, y
+    pix = img.load()
+    vertical = find_vertical_border(width, height, pix)
+    horizontal = find_horizontal_border(width, height, pix)
+    points = []
+    for v in range(0, len(vertical), 2):
+        location = (horizontal[0], vertical[v], horizontal[1], vertical[v + 1])
+        points.append(location)
+    return points
+
+
+def assert_one_line_width(h, width, pix):
+    for w in range(width - 1):
+        if pix[w, h] != pix[w + 1, h]:
+            return True
+    return False
+
+
+def assert_one_line_height(w, height, pix):
+    for h in range(height - 1):
+        if pix[w, h] != pix[w, h + 1]:
+            return True
+    return False
+
+
+def find_horizontal_border(width, height, pix):
+    horizontal = []
+    flag = True
+    for w in range(width):
+        if assert_one_line_height(w, height, pix):
+            if flag:
+                flag = False
+                horizontal.append(w)
+        else:
+            if not flag:
+                horizontal.append(w)
+                flag = True
+    if len(horizontal) == 1:
+        horizontal.append(width - 1)
+    return horizontal
+
+
+def find_vertical_border(width, height, pix):
+    vertical = []
+    flag = True
+    for h in range(height):
+        if assert_one_line_width(h, width, pix):
+            if flag:
+                flag = False
+                vertical.append(h)
+        else:
+            if not flag:
+                vertical.append(h)
+                flag = True
+    if len(vertical) % 2:
+        vertical.append(height - 1)
+    return vertical
+
+print(find_images_location_from_url('http://black-up.kr/web/upload/200703_hj_nr9_sk_01.jpg'))
